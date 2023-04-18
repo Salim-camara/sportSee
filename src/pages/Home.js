cc4a4235cf060c17aacf2fe2823db57c5c09057a
Salim Camara <m.salimcamara@gmail.com>
2023-04-18 11:50:04 +0200

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [logements, setlogements] = useState();
+
+  return (
+    <div>
+      <p>hello ! :)</p>
+    </div>
+  );
+};
+
+export default Home;
