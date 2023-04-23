c0fd2a774012816aa34d3551efb6e240ab310c57
Salim Camara <m.salimcamara@gmail.com>
2023-04-23 14:04:07 +0200

    feat(leftMenu): Ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,35 @@ const Home = () => {
         </div>
       </div>
       <div className="home__content">
-        <div className="home__content__menu"></div>
+        <div className="home__content__menu">
+          <div className="home__content__menu__btn">
+            <div className="home__content__menu__btn--global">
+              <img
+                src={require("./../assets/images/menu_1.png")}
+                className="home__content__menu__btn--first"
+              />
+            </div>
+            <div className="home__content__menu__btn--global">
+              <img
+                src={require("./../assets/images/menu_2.png")}
+                className="home__content__menu__btn--second"
+              />
+            </div>
+            <div className="home__content__menu__btn--global">
+              <img
+                src={require("./../assets/images/menu_3.png")}
+                className="home__content__menu__btn--third"
+              />
+            </div>
+            <div className="home__content__menu__btn--global">
+              <img
+                src={require("./../assets/images/menu_4.png")}
+                className="home__content__menu__btn--forth"
+              />
+            </div>
+          </div>
+          <p className="home__content__menu--text">Copirygth SportSee 2020</p>
+        </div>
         <div className="home__content__board"></div>
       </div>
     </div>
